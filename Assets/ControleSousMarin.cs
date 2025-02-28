99d8591be8c7252d21ff7fe9ac0edb183d6027e7
Yeva Vudz <yevavudz545@gmail.com>
2025-02-27 22:44:54 -0500

    SousMarin capable d'aller en haut et en bas. deplacement en y.

--- Assets/ControleSousMarin.cs
+++ Assets/ControleSousMarin.cs
@@ -9,44 +9,39 @@ public class ControleSousMarin : MonoBehaviour
     [SerializeField] private float _vitesseMouvement;
     private Rigidbody _rb;
     private Vector3 directionInput;
+    private Animator _animator;
+    private ControleSousMarin _mouvementSousMarin;
 
     [SerializeField] private float _modifierAnimTranslation;
-    private Animator _animator;
-    private float _rotationVelocity;
 // Start is called before the first frame update
 void Start()
     {
     _rb = GetComponent<Rigidbody>();
     _animator = GetComponent<Animator>();
-}
+    _mouvementSousMarin = GetComponent<ControleSousMarin>();
+    }
 
-    void OnMouvement(InputValue directionBase)
+    void OnAvantArriere(InputValue directionBase)
+    {
+        Vector3 directionAvecVitesse = directionBase.Get<Vector3>() * _vitesseMouvement;
+        directionInput = new Vector3(0f, directionAvecVitesse.z, 0f);
+    }
+    void OnHautBas(InputValue directionBase)
     {
         Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _vitesseMouvement;
-        directionInput = new Vector3(directionAvecVitesse.x, 0f, directionAvecVitesse.y);
+        directionInput = new Vector3 (directionAvecVitesse.x, 0f, directionAvecVitesse.y);
     }
 
     void FixedUpdate()
     {
         // calculer et appliquer la translation
-        Vector3 mouvement = directionInput;
-        float rotation = 0f;
-        // si on a une direction d'input
-        if (directionInput.magnitude > 0f)
-        {
-            // calculer rotation cible
-            float rotationCible = Vector3.SignedAngle(-Vector3.forward, directionInput, Vector3.up);
-            // faire le changement plus graduel avec une interpolation
-            rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationCible, ref _rotationVelocity, 0.12f);
-            // appliquer la rotation cible directement
-            _rb.MoveRotation(Quaternion.Euler(0.0f, rotation, 0.0f));
-        }
-        // appliquer la vitesse de translation
-        _rb.AddForce(mouvement, ForceMode.VelocityChange);
+        Vector3 mouvement = _mouvementSousMarin.directionInput;
+        _rb.velocity = mouvement;
+      
 
         // calculer un modifiant pour la vitesse d'animation
-        Vector3 vitesseSurPlane = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
-        _animator.SetFloat("Vitesse", vitesseSurPlane.magnitude * _modifierAnimTranslation);
-        _animator.SetFloat("Deplacement", vitesseSurPlane.magnitude);
+        
+        _animator.SetFloat("Vitesse", mouvement.magnitude * _modifierAnimTranslation);
+       
     }
 }
